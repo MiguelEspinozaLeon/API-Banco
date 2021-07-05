52aef15b3ecc915013cc914daf34364278ba882a
Miguel Espinoza <miguel.espinozaleon@gmail.com>
2021-07-04 21:50:48 -0700

    errores arreglados

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ const app = express();
 
 
 app.use(express.json());
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 //MySQL
 
 // var connection = mysql.createConnection({
